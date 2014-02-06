63f6e9eaaf59d007f7f777f5850e1667c6131ffc
Terrence Howard <chemisus@gmail.com>
2014-02-05 19:08:42 -0500

    made callConstraint method

--- src/DatabaseTable.js
+++ src/DatabaseTable.js
@@ -16,37 +16,24 @@ function DatabaseTable() {
         return constraints[constraint_type];
     };
 
+    this.callConstraint = function (method, database, constraints, table_name, record) {
+        for (var i in database.constraint_keys) {
+            var constraint_data = this.getConstraintData(database, i);
+            var constraint = this.getConstraint(database, constraint_data, constraints);
+
+            constraint[method](database, constraint_data, table_name, record);
+        }
+    };
+
     this.insert = function (database, constraints, table_name, record) {
         var table = database.tables[table_name];
 
-        for (var i in database.constraint_keys) {
-            var constraint_data = this.getConstraintData(database, i);
-            var constraint = this.getConstraint(database, constraint_data, constraints);
-
-            constraint.preDatabaseInsert(database, constraint_data, table_name, record);
-        }
-
-        for (var i in database.constraint_keys) {
-            var constraint_data = this.getConstraintData(database, i);
-            var constraint = this.getConstraint(database, constraint_data, constraints);
-
-            constraint.preTableInsert(database, constraint_data, table_name, record);
-        }
+        this.callConstraint('preDatabaseInsert', database, constraints, table_name, record);
+        this.callConstraint('preTableInsert', database, constraints, table_name, record);
 
         table.records.push(record);
 
-        for (var i in database.constraint_keys) {
-            var constraint_data = this.getConstraintData(database, i);
-            var constraint = this.getConstraint(database, constraint_data, constraints);
-
-            constraint.postTableInsert(database, constraint_data, table_name, record);
-        }
-
-        for (var i in database.constraint_keys) {
-            var constraint_data = this.getConstraintData(database, i);
-            var constraint = this.getConstraint(database, constraint_data, constraints);
-
-            constraint.postDatabaseInsert(database, constraint_data, table_name, record);
-        }
+        this.callConstraint('postTableInsert', database, constraints, table_name, record);
+        this.callConstraint('postDatabaseInsert', database, constraints, table_name, record);
     };
 }
